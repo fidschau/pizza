163eddf02ea2074d43bb9043b885662998769643
Providence Ncube <providencen@gmail.com>
2017-02-23 17:49:12 +0200

    Implimented The read file and converted the char to String

--- src/com/gnac/Main.java
+++ src/com/gnac/Main.java
@@ -3,14 +3,19 @@ package com.gnac;
 import com.gnac.models.Cell;
 import com.gnac.models.Pizza;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Objects;
+import java.util.stream.Stream;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
        // System.out.println("Booinux un");
 
-        int [] pizzaDescription = getPizzaDescriptionFromFile("small.in");
-        char [][] rawArray = getRawArrayFromFile("small.in");
+        String[][] rawArray = getRawArrayFromFile("small.in");
+        int[] pizzaDescription = Stream.of(rawArray[0]).mapToInt(Integer::parseInt).toArray();
         Pizza pizza= createPizzaFromArray(pizzaDescription,rawArray);
         pizza.cutSlices();
         String submissionString = pizza.getSubmissionString();
@@ -25,16 +30,16 @@ public class Main {
      * @param rawArray
      * @return
      */
-    public static Pizza createPizzaFromArray(int [] pizzaDescription,char [][] rawArray){
+    public static Pizza createPizzaFromArray(int [] pizzaDescription,String [][] rawArray){
         Pizza pizza;
-        char tomato = 'T';
+        String tomato = "T";
         if (pizzaDescription.length==4){
             pizza = new Pizza(pizzaDescription);
 
             for (int x=0; x<pizza.getRows();x++){
                 for (int y=0; y<pizza.getColumns();y++){
-                    char c= rawArray[x][y];
-                    Cell cell = new Cell(c==tomato,x,y);
+                    String c = rawArray[x][y];
+                    Cell cell = new Cell(Objects.equals(c, tomato),x,y);
                     pizza.addCell(cell);
                 }
             }
@@ -62,10 +67,28 @@ public class Main {
      * @param fileName
      * @return
      */
-    public static char [][] getRawArrayFromFile(String fileName){
-        //Todo
-        char [][] rawArray = new char[14][45];
-        return rawArray;
+    public static String[][] getRawArrayFromFile(String fileName){
+        try {
+            int count = 0;
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            String[] imageLine = reader.readLine().split("\\s");
+            int row = Integer.parseInt(imageLine[0]);
+            int column = Integer.parseInt(imageLine[1]);
+
+            String[][] image = new String[row][column];
+            while (count < column) {
+                String line = reader.readLine();
+                if (line == null) {
+                    break;
+                }
+                String[] temp = line.split("(?!^)");
+                image[count] = temp;
+                count++;
+            }
+            return image;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     /**
